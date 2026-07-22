fcfdbe90c7432a4b91fac8d5ee01f0ea1b819e1a
DEEPIKA T <deepikatg07@gmail.com>
2026-07-22 19:20:12 +0530

    Create problem1.java

---
+++ Day12/problem1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+         Arrays.sort(nums);
+        int i=0;
+        while(i<nums.length)
+        {
+            if(nums[i] == i){
+                i++;
+            }
+            else{
+                return i;
+            }
+        }
+        return i;
+    }
+}
